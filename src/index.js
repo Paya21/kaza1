a0c2875c91c17fa31b7414802812724610e097a8
Clement Paya <clementpaya21@gmail.com>
2023-03-31 19:29:44 +0200

    init Routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import Home from './page/Home';
@@ -12,12 +13,17 @@ import Cards from './page/Cards'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <Home />
+    <Routes>
       <Header />
-      <About />
+      <Route exact path='/'>
+        <Home />
+      </Route>
+      <Route path='/about'>
+        <About />
+      </Route>
       <Cards />
-    </div>
+    </Routes>
+    
   </React.StrictMode>
 );
 
